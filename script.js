f586096bb594265e20f38c57c8367f591ef0f805
Joe Wills <JGW3Code@gmail.com>
2020-06-01 21:15:51 -0500

    got a lot working, just a little more to do

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-moment().format('MMMM Do YYYY, h:mm:ss a');
+$(document).ready(function () {
+  var saveBtn = $(".saveBtn");
+  moment().format("MMMM Do YYYY, h:mm:ss a")
+  saveBtn.on("click", function () {
+    var text = $(this).siblings(".description").val();
+    let time = $(this).parent().attr("id");
+    localStorage.setItem(time, text);
+  });
+  $('#currentDay').text( moment().format("MMMM Do YYYY, h:mm:ss a"));
+  $("#hour-9 .description").val(localStorage.getItem('hour-9'))
+
+
+
+
+  
+});
+
